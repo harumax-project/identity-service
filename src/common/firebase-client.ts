35de167aa7abd978316af41aba1d0579bcaf7aba
maxkenkomoda <tango12speedcell@gmail.com>
2021-11-19 18:09:33 +0900

    fix firebase config setting

--- src/common/firebase-client.ts
+++ src/common/firebase-client.ts
@@ -13,10 +13,19 @@ const firebaseConfig = {
 
 export class FirebaseClient {
   private firebaseApp: FirebaseApp
-  private firebaseConfigData = firebaseConfig
+  private firebaseConfigData = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MESUREMENT_ID,
+  }
 
   constructor() {
-    this.firebaseApp = initializeApp(firebaseConfig)
+    this.firebaseApp = initializeApp(this.firebaseConfigData)
   }
 
   get app(): FirebaseApp {
