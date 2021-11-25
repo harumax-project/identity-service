e711fb4bbdbcd1a9f17a1325b0fd5203562e4a51
maxkenkomoda <tango12speedcell@gmail.com>
2021-11-25 17:08:56 +0900

    create session cookie

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,23 +1,47 @@
-import { Body, Controller, Get, Post, Res } from '@nestjs/common'
+import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common'
 import { AuthService } from './auth.service'
-import { Response } from 'express'
-import { FIREBASE_CLIENT } from 'src/main'
+import { Request, Response } from 'express'
+import { FIREBASE_ADMIN, FIREBASE_CLIENT } from 'src/main'
+import { FirebaseAdmin } from 'src/common/firebase-admin'
+import { FirebaseClient } from 'src/common/firebase-client'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Get()
-  root(@Res() res: Response) {
+  signInWithFirebaseUi(@Res() res: Response) {
     return res.render(
       `${process.env.HTML_NAME}`,
       {
         title: 'Identity',
         firebaseConfig: FIREBASE_CLIENT.firebaseConfig,
-        signInRedirectURL: 'http://localhost:3000/auth',
+        signInRedirectURL: `${process.env.SIGN_IN_REDIRECT_URL}`,
+        sessionLogin: `${process.env.IDENTITY_SERVICE_URL}/sessionLogin`,
       },
       (err, html) => {
         res.send(html)
+      },
+    )
+  }
+
+  @Get('sessionLogin')
+  async sessionLogin(@Req() req: Request, @Res() res: Response) {
+    const idToken = req.headers.authorization.split(' ')[1]
+    const csrfToken = await this.authService.createCsrfToken(req)
+    const expiresIn = 60 * 60 * 24 * 5 * 1000
+    const options = { maxAge: expiresIn, httpOnly: true, secure: true }
+    // console.log(csrfToken)
+    // res.cookie('csrfToken', csrfToken, options)
+    FIREBASE_ADMIN.auth.createSessionCookie(idToken, { expiresIn }).then(
+      (sessionCookie) => {
+        res.cookie('__session', sessionCookie, options)
+        res.cookie('csrfToken', csrfToken, options)
+        res.end(JSON.stringify({ status: 'success' }))
+      },
+      (error) => {
+        res.status(401).send('UNAUTHORIZED REQUEST!')
+        console.log(error)
       },
     )
   }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,30 @@
 import { Injectable } from '@nestjs/common'
-import { EmailAndPassword } from './dto/auth.dto'
 import { FirebaseClient } from 'src/common/firebase-client'
 import { FirebaseApp } from '@firebase/app'
 import { FirebaseAdmin } from 'src/common/firebase-admin'
+import * as jwt from 'jsonwebtoken'
+import jwt_decode from 'jwt-decode'
+
+import { auth } from 'google-auth-library'
+import { Request } from 'express'
+import { DecodedJWTToken } from './dto/auth.dto'
 
 @Injectable()
 export class AuthService {
-  // private app: FirebaseApp
+  public decodeIdToken(req: Request) {
+    const authHeader = req.headers.authorization
+    if (!authHeader) throw new Error('no authHeader')
+    return jwt_decode(authHeader.split(' ')[1])
+  }
+
+  public async createCsrfToken(req: Request) {
+    const decodedToken = this.decodeIdToken(req) as DecodedJWTToken
+    const uid = decodedToken.user_id
+
+    const payload = { uid }
+    const creds = await auth.getCredentials()
+    const privateKey = creds.private_key
+    const csrfToken = jwt.sign(payload, privateKey, { algorithm: 'RS256' })
+    return csrfToken as string
+  }
 }
