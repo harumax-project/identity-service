3f07abf7e6b7482355762947d539e607bc69b8e6
maxkenkomoda <tango12speedcell@gmail.com>
2022-01-01 00:43:48 +0900

    add endpoint

--- src/functions/users/users.controller.ts
+++ src/functions/users/users.controller.ts
@@ -1,15 +1,7 @@
-import {
-  Controller,
-  Get,
-  Query,
-  Response,
-  UseGuards,
-  ValidationPipe,
-} from '@nestjs/common'
+import { Controller, Get, Response, UseGuards } from '@nestjs/common'
 import { GuardResponse } from 'src/common-functions/decorators/guard-response'
 import { AuthGuard } from 'src/guards/auth.guard'
 import { UserInfoDto } from './users-dto/user-info.dto'
-import { UserQueryDto } from './users-dto/users-query.dto'
 import { UsersService } from './users.service'
 
 @Controller('users')
@@ -18,11 +10,7 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  getUsers(
-    @Query(ValidationPipe) query: UserQueryDto,
-    @GuardResponse() userInfo: UserInfoDto,
-    @Response() res,
-  ) {
-    res.send('success')
+  getUsers(@GuardResponse() userInfo: UserInfoDto, @Response() res) {
+    res.send({ status: 'success' })
   }
 }
